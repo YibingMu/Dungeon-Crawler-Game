63e60deafd35f1ae7607a0084fb609345854c84a
穆 一冰 <bokuyihi@guest-wireless-upc-1603-10-120-060-197.usc.edu>
2018-02-28 14:31:03 -0800

    Almost Finish

---
+++ src/features/map/index.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import {SPRITE_SIZE} from "../../config/constant";
+import './style.css'
+import { connect } from 'react-redux'
+
+function getTileSprite(type) {
+    switch (type) {
+        case 0:
+            return 'grass'
+        case 3:
+            return 'tree'
+        case 4:
+            return 'chest'
+        case 5:
+            return 'rock'
+        case 6:
+            return 'tree'
+        default:
+            return 'grass'
+    }
+}
+
+function MapTile(props) {
+    return (<div className={`tile ${getTileSprite(props.tile)}`}
+        style={{
+            height: SPRITE_SIZE,
+            width: SPRITE_SIZE
+        }}
+    >
+        {props.tile}
+    </div>)
+}
+
+function MapRow(props) {
+    return <div className="row">
+        {
+            props.tiles.map(tile => <MapTile tile={tile}/>)
+        }
+    </div>
+}
+
+function Map(props){
+    return(
+        <div
+            style={{
+                position: 'relative',
+                top: '0px',
+                left: '0px',
+                width: '800px',
+                height: '480px',
+                border: '4px solid white',
+            }}
+        >
+            {
+                props.tiles.map(row => <MapRow tiles={row}/>)
+            }
+        </div>
+    )
+}
+
+function mapStateToProps(state) {
+    return {
+        tiles: state.map.tiles
+    }
+}
+
+export default connect(mapStateToProps)(Map)
---
+++ src/features/world/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Player from '../player'
+import Map from '../map'
+import { tiles } from '../../data/maps/1'
+import store from '../../config/store'
+
+function World(props){
+    store.dispatch({type: 'ADD_TILES', payload: {
+         tiles: tiles
+        }})
+    return(
+        <div
+        style={{
+            position: 'relative',
+            width: '800px',
+            height: '480px',
+            margin: '20px auto'
+        }}
+        >
+            <Map tiles={tiles}/>
+            <Player/>
+        </div>
+    )
+}
+
+export default World
